56a46d87444f3e6d9e64f46540bc94ae5e8eb3e6
Paul Ndambo <52633424+Paulndambo@users.noreply.github.com>
2023-08-30 15:05:08 +0300

    dockerfile updated to add gunicorn

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from apps.users.models import User, Customer, Influencer
 # Register your models here.
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id", "username", "role", "email"]
+
 admin.site.register(Customer)
-admin.site.register(Influencer)
+
+@admin.register(Influencer)
+class InfluencerAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "phone_number"]
---
+++ load_testing.py
@@ -0,0 +1,19 @@
+import asyncio
+import aiohttp
+
+async def make_request(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+async def main():
+    url = 'http://127.0.0.1:8000/analytics/promotion-campaigns/'  # Replace with the API endpoint URL
+    num_requests = 1000000
+
+    async with aiohttp.ClientSession() as session:
+        tasks = [make_request(session, url) for _ in range(num_requests)]
+        responses = await asyncio.gather(*tasks)
+        print(responses)
+
+    # Process responses or log them as needed
+
+asyncio.run(main())
