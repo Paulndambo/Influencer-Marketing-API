5351202748098839a5194493cada3e8c9b121f7d
Paul Ndambo <52633424+Paulndambo@users.noreply.github.com>
2023-09-07 11:38:02 +0300

    divide by zero removed on revenue distribution

--- InfluencerMarketer/celery.py
+++ InfluencerMarketer/celery.py
@@ -3,11 +3,12 @@ import os
 from celery import Celery
 from django.conf import settings
 
-#cloud_amqp = "amqps://qiqdvcyl:KPtRQ1jnfpuF7NNuAaVi9tynarNPc6XH@hummingbird.rmq.cloudamqp.com/qiqdvcyl"
+cloud_amqp = "amqps://qiqdvcyl:KPtRQ1jnfpuF7NNuAaVi9tynarNPc6XH@hummingbird.rmq.cloudamqp.com/qiqdvcyl"
 #cloud_amqp = "amqp//guest:guest@rabbitmq:5672/"
+DOCKER_COMPOSE_MQ = "amqp://admin:admin@rabbitmq"
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "InfluencerMarketer.settings")
 
-app = Celery("InfluencerMarketer", broker="amqp://admin:admin@rabbitmq")
+app = Celery("InfluencerMarketer", broker=cloud_amqp)
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
--- apps/payments/distribute_promotion_budget.py
+++ apps/payments/distribute_promotion_budget.py
@@ -23,15 +23,23 @@ def calculate_and_distribute_payment(product: Product):
     elif product.revenue_distributed == False and product.campaign_limit_reached == True:
         campaigns = product.productcampaigns.all()
 
+        if not campaigns:
+            return
+
         for campaign in campaigns:
             influencer = campaign.influencer
             # Calculate payment based on engagement metrics and product budget.
             total_campaign_engagement = campaign.clicks
             total_product_engagement = sum(product.productcampaigns.values_list("clicks", flat=True))
 
-            engagement_percentage = Decimal(total_campaign_engagement) / Decimal(total_product_engagement) 
-            payment_amount = Decimal(product.promotion_budget) * Decimal(engagement_percentage)
+            print(f"Campaign Engagement Type: {total_campaign_engagement}")
+            print(f"Product Engagement Type: {total_product_engagement}")
 
+            payment_amount = 0
+            if total_campaign_engagement > 0:
+                engagement_percentage = total_campaign_engagement / total_product_engagement 
+                payment_amount = Decimal(product.promotion_budget) * Decimal(engagement_percentage)
+            
             # Distribute the payment to the influencer's wallet.
             wallet = Wallet.objects.filter(user=influencer.user).first()
             if not wallet:
